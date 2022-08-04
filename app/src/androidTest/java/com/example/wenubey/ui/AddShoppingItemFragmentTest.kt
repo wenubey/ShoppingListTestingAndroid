cdca91655bb91dcd7a7217e069d0ed754dfa3768
wenubey <mertfatihsimsek06@gmail.com>
2022-08-05 00:52:09 +0200

    second commit

--- app/src/androidTest/java/com/example/wenubey/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/example/wenubey/ui/AddShoppingItemFragmentTest.kt
@@ -1,19 +1,29 @@
 package com.example.wenubey.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.example.wenubey.launchFragmentInHiltContainer
+import com.example.wenubey.repositories.FakeShoppingRepositoryAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
+import com.example.wenubey.R
+import com.example.wenubey.data.local.ShoppingItem
+import com.example.wenubey.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
 
 @MediumTest
 @HiltAndroidTest
@@ -23,9 +33,33 @@ class AddShoppingItemFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     @Before
     fun setup() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewModel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(
+            fragmentFactory = fragmentFactory
+        ) {
+            viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5.5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem("shopping item", 5, 5.5f, ""))
     }
 
     @Test
